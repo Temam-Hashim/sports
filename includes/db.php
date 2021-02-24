f5f0c808b8d1514b65f714473e9176cea1f20fc1
Temam-Hashim <temamhashim3@gmail.com>
2021-02-24 17:55:43 +0530

    db transfer
    database transferred to remote  server

--- includes/db.php
+++ includes/db.php
@@ -1,14 +1,18 @@
 <?php
- $username = "root";
- $password="";
- $db="sports";
- $host = "localhost";
- $connect = mysqli_connect($host,$username,$password,$db);
+///local server
+ //$username = "root";
+ //$password="";
+ //$db="sports";
+// $host = "localhost";
+ //$connect = mysqli_connect($host,$username,$password,$db);
 
-//  if($connect){
-//    echo "connection done";
 
-//  }else{
-//    echo "connection failed";
-//  }
+///remote server
+ $username = "bPWeE5vC1C";
+ $password="k9uS8mkZn8";
+ $db="bPWeE5vC1C";
+ $host = "remotemysql.com";
+ $port = 3306;
+ $connect = mysqli_connect($host,$username,$password,$db,$port);
+
 ?>
