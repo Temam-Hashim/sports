37a5999eb8aaf5e3a188b6f2dad0eacfeadac204
Temam-Hashim <temamhashim3@gmail.com>
2021-02-28 01:32:11 +0530

    db switch
    switch db to remote

--- includes/db.php
+++ includes/db.php
@@ -1,18 +1,18 @@
 <?php
 ///local server
-  $username = "root";
-  $password="";
-  $db="sports";
-  $host = "localhost";
-  $connect = mysqli_connect($host,$username,$password,$db);
+  // $username = "root";
+  // $password="";
+  // $db="sports";
+  // $host = "localhost";
+  // $connect = mysqli_connect($host,$username,$password,$db);
 
 
 ///remote server
-//  $username = "bPWeE5vC1C";
-//  $password="k9uS8mkZn8";
-//  $db="bPWeE5vC1C";
-//  $host = "remotemysql.com";
-//  $port = 3306;
-//  $connect = mysqli_connect($host,$username,$password,$db,$port);
+ $username = "bPWeE5vC1C";
+ $password="k9uS8mkZn8";
+ $db="bPWeE5vC1C";
+ $host = "remotemysql.com";
+ $port = 3306;
+ $connect = mysqli_connect($host,$username,$password,$db,$port);
 
 ?>
